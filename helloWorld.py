51e0169feb41d6fb40479fb44844daf317a72f7d
rhdolin <bdolin@elimu.io>
2022-04-27 07:37:31 -0700

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -29,7 +29,7 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=12, range=(0,24))[0]
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
